42cbf5df4489e5c7273c870f981e0368909ff69c
Rafael Santana <rafael.santana@webjump.com.br>
2022-02-21 18:04:43 -0300

    feat: summary component

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display:flex;
+  justify-content: space-around;
+  margin-top: -10rem;
+
+  div{
+    width: 22rem;
+    background-color: var(--shape);
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: var(--text-title)
+
+    header{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    strong{
+      display: block;
+      margin-top: 1rem;
+      font-size: 2rem;
+      font-weight: 500;
+      line-height: 3rem
+    }
+
+    &.highlight-background{
+      background-color: var(--green);
+      color: #fff
+    }
+  }
+`;
